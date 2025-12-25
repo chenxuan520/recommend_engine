168db95918c7bc287836a7b9921797e214a13198
chenxuan <1607772321@qq.com>
2025-12-25 16:02:49 +0800

    feat: add CORS middleware support

--- internal/server/server.go
+++ internal/server/server.go
@@ -31,8 +31,25 @@ func NewServer(up user.Provider, engine *workflow.Engine, hs history.Store) *Ser
 		engine:       engine,
 		historyStore: hs,
 	}
+	s.router.Use(s.corsMiddleware())
 	s.setupRoutes()
 	return s
+}
+
+func (s *Server) corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 // Run 启动服务器
